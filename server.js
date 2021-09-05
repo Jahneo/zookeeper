2b202feb24cacbffe480766c2aa7b7d8ba1b66a2
Jahneo <85087102+Jahneo@users.noreply.github.com>
2021-09-05 01:10:27 -0400

    add server and add heroku

--- server.js
+++ server.js
@@ -43,6 +43,10 @@ app.listen(PORT, () => {
     // return the filtered results:
     return filteredResults;
   }
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
   app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -50,3 +54,11 @@ app.listen(PORT, () => {
     }
     res.json(results);
   });
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+      } else {
+        res.send(404);
+      }
+    });
